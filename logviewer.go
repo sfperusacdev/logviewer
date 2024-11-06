456619148cdec4365c12c1f57a7daf978dd4c9b5
Kevin Sacuedo <user002_@outlook.com>
2024-11-06 09:39:54 -0500

    add logs

--- logviewer.go
+++ logviewer.go
@@ -2,6 +2,7 @@ package logviewer
 
 import (
 	"database/sql"
+	"log"
 
 	"fmt"
 	"io"
@@ -25,6 +26,7 @@ func NewMemoryLogger() (MemoryLogger, error) {
 	var sqlitePath = ":memory:"
 	db, err := sql.Open("sqlite", sqlitePath)
 	if err != nil {
+		slog.Error("sqlite log db", "error", err)
 		return nil, err
 	}
 	if _, err := db.Exec("create table logs(value text)"); err != nil {
@@ -40,6 +42,9 @@ func (l *sqliteLogger) Close() error { return l.db.Close() }
 func (l *sqliteLogger) Write(p []byte) (int, error) {
 	const qry = "insert into logs(value) values ('%s')"
 	var _, err = l.db.Exec(fmt.Sprintf(qry, string(p)))
+	if err != nil {
+		log.Println("insert log ERROR", err)
+	}
 	return len(p), err
 }
 
@@ -47,6 +52,7 @@ func (l *sqliteLogger) ReadLines() ([]string, error) {
 	const qry = "select * from logs"
 	rows, err := l.db.Query(qry)
 	if err != nil {
+		log.Println("select logs ERROR", err)
 		return nil, err
 	}
 	defer rows.Close()
